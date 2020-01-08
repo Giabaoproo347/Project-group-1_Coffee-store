2cf13f3d438ec1ebf5e1545e495462f95d4f7014
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2020-01-08 20:45:17 +0700

    thêm Application Config

---
+++ coffee-server/src/main/java/com/project/coffee/config/ApplicationConfig.java
@@ -0,0 +1,34 @@
+package com.project.coffee.config;
+
+import com.project.coffee.service.*;
+import com.project.coffee.service.impl.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl();
+    }
+
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentServiceImpl();
+    }
+
+    @Bean
+    public CategoriesService categoriesService() {
+        return new CategoriesServiceImpl();
+    }
+
+    @Bean
+    public ProductService productService() {
+        return new ProductServiceImpl();
+    }
+
+    @Bean
+    public PromotionService promotionService() {
+        return new PromotionServiceImpl();
+    }
+}
